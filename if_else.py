632fba00c7c1fb7c1f44d77dcebcf312ddea4813
Tanya Ryzhova <rtanyas@gmail.com>
2018-01-10 10:27:20 +0300

    fix bug

--- if_else.py
+++ if_else.py
@@ -1,6 +1,6 @@
-MODULE1 = "test1"
-MODULE2 = "test2"
+MODULE1 = "test_module1"
+MODULE2 = "test_module2"
 version = (6, 7)
 
-service = (const.MODULE1 if version < (6, 10)
-           else .MODULE2)
+service = (MODULE1 if version < (6, 10)
+           else MODULE2)
